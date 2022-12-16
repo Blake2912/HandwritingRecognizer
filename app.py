3db8c6d120c5355b6905a4bf60c51bfbed75b5bf
Blake2912 <kcvarun29122001@gmail.com>
2022-12-17 01:01:14 +0530

    Refactor and updates

--- app.py
+++ app.py
@@ -54,21 +54,21 @@ def infer(model: Model, fn_img: Path) -> None:
     print(f'Probability: {probability[0]}')
     print("After Sentence checking...")
     corrected_spelling = TextBlob(recognized[0])
-    return corrected_spelling.correct()
+    return [recognized[0],corrected_spelling.correct()]
 
 @app.route("/", methods=['GET', 'POST'])
 def show_page():
     if request.method == 'POST':
         if 'file1' not in request.files:
-            return render_template('index.html',status='there is no file1 in form!',result='')
+            return render_template('index.html',status='there is no file1 in form!',result='',found='')
         file1 = request.files['file1']
         try:
             path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
             file1.save(path)
             model = Model(char_list_from_file(), 0, must_restore=True, dump=False)
             res = infer(model, path)
-            return render_template('index.html',status='File Uploaded Successfully',result="Sentence Predicted from Image: {0}".format(res))
+            return render_template('index.html',status='File Uploaded Successfully',found="Sentence Predicted from Image: {0}".format(res[0]),result="Corrected Sentence after spell checking: {0}".format(res[1]))
         except:
-            return render_template('index.html',status='Please Upload the file Properly')
+            return render_template('index.html',status='Please Upload the file Properly',found='',result='')
     
     return render_template('index.html',status='Upload a File',result='')
